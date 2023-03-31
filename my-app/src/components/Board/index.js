7c3d070ad108beaab3fe3197bcf26ea492b38c74
lucasFilppe <lucasfilipe2323@icloud.com>
2023-03-30 23:22:31 -0300

    declarando um vencedor

--- my-app/src/components/Board/index.js
+++ my-app/src/components/Board/index.js
@@ -10,13 +10,47 @@ export default function Board() {
   si e com seus pais. */
   const  [squares, setSquares] = useState(Array(9).fill(null));//Array(9).fill(null)cria uma matriz com nove elementos e define cada um deles como null
 
-  //estado para movimento do jpgo
+  //estado para movimento do jogo
   const [xIsNext, setXIsNext] = useState(true);
+
+  //mostrando jogadas e o vencedor
+  const winner = calculateWinner(squares)
+  let status;
+  if(winner){
+    status = "Winner " + winner;
+  }
+  else{
+    status = "New player " + (xIsNext ? "X": "O")
+  }
+
+  //função que declara um vencedor
+  function calculateWinner(){
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+
+    for(let i = 0; i < lines.length; i++){
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a]
+      }
+    }
+    return null;
+  }
+
+  
 
   //função que atualiza quadrado
   function handleClick(i){
 
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return
     }
 
@@ -34,6 +68,7 @@ export default function Board() {
 
   return (
     <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
       <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
