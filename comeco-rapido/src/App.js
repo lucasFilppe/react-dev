13b646356400e02b8d33b6a60e6d2d3f969b7e12
lucasFilppe <lucasfilipe2323@icloud.com>
2023-04-06 12:04:38 -0300

    exibindo dados

--- comeco-rapido/src/App.js
+++ comeco-rapido/src/App.js
@@ -1,10 +1,12 @@
 import Button from "./components/Button";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div>
       <h1>Começando com React</h1>
       <Button/>
+      <Profile/>
     </div>
   );
 }
---
+++ comeco-rapido/src/components/Profile/index.js
@@ -0,0 +1,25 @@
+import '../Profile/styles.css';
+
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90
+};
+
+function Profile() {
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </div>
+  );
+}
+export default Profile;
